30499a82618c2341b2f67ffabb1d5f5f5e89165e
JD <jharalson29@gmail.com>
2022-01-09 21:28:10 -0500

    Notes added to JS file

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// when onlclick a martial art on the main page it takes you to the definition page
+// when hovering a martial art a picture is displayed
+
+// build a form for contacts to submit information
+
+// link to API to show all upcoming grappling and fighting events - create a separate page to show this on
+// can use moment.js to show how long until next event
+
+// need an MMA event API
